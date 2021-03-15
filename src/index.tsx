ce23b5305a284a0c554562dd35bda09f0f430ff9
Alexander Daily <Alexander.Daily@ics-ag.de>
2021-03-15 18:07:46 +0100

    init src directory

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+import './index.css'
+import { config } from './utils/config'
+
+function App(): JSX.Element {
+  return (
+    <Router>
+      <Routes>
+        <Route path={config.routes.home} element={<Home />} />
+      </Routes>
+    </Router>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
